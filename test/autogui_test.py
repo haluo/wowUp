6cab87c3e0885a9449f620c7aa1757b8eef153c0
zhufeng <zhufeng3612@163.com>
2021-05-29 10:32:04 +0800

    pyautogui

--- test/autogui_test.py
+++ test/autogui_test.py
@@ -1,11 +1,17 @@
 import pyautogui
 import win32gui
+import time
 
 
 
 hwnd = win32gui.FindWindow(None, '微信')
 print(hwnd)
-win32gui.SetForegroundWindow(hwnd)
+
+if(win32gui.IsIconic(hwnd)):#如果最小化则激活并展示
+    win32gui.ShowWindow(hwnd, 9)
+    time.sleep(0.5)
+
+win32gui.SetForegroundWindow(hwnd)  #展示到最前
 
 
 img_location = pyautogui.locateOnScreen(image='img.png')
